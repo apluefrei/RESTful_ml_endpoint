e312d962f7c676562b474ac0b12e4950f041a526
Paul BERTRAND <72377576+apluefrei@users.noreply.github.com>
2023-02-05 02:31:00 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,16 +5,13 @@ class ClassifyTestCase(unittest.TestCase):
     
     def create_app(self):
         return app
-    
-    def setUp(self):
-        self.url = "http://localhost:5000/classify"
 
     def test_classify(self):
         # Sample pixels data
         pixels = [0 for i in range(784)]
         data = {'pixels': pixels}
 
-        response = requests.post(self.url, json=data)
+        response = requests.post("http://localhost:5000/classify", json=data)
         expected_result = {"class": '0'}
 
         assert result == expected_result, f"Expected {expected_result} but got {result}"
