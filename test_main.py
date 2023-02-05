1739da818622681f69279767c7ff5fc6ffc106f7
Paul BERTRAND <72377576+apluefrei@users.noreply.github.com>
2023-02-05 19:01:18 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,9 +12,10 @@ class ClassifyTestCase(unittest.TestCase):
         data = {'pixels': pixels}
 
         response = requests.post("http://localhost:5000/classify", json=data)
+        answer = response.json()
         expected_result = {"class": '0'}
 
-        assert result == expected_result, f"Expected {expected_result} but got {response.json()}"
+        assert result == expected_result, f"Expected {expected_result} but got {answer}"
         print("Test passed!")
 
 if __name__ == '__main__':
